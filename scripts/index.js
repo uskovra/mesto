8438dc0bbc49ff4fda493041d2aeea82309dcdd2
rauskov <uskov.ra@yandex.ru>
2022-02-13 15:57:55 +0800

    первая сборка

---
+++ scripts/index.js
@@ -0,0 +1,61 @@
+// хидер, контент, футер
+const header = document.querySelector('.header');
+const content = document.querySelector('.content');
+const footer = document.querySelector('.footer');
+
+// попап 
+const popup = document.querySelector('.popup');
+
+// кнопка закрытия попапа 
+const popupCloseButton = popup.querySelector('.popup__btn_action_close');
+
+// поля формы
+const popupItems = popup.querySelectorAll('.popup__item');
+
+// кнопка сохранения формы 
+const popupSubmitButton = popup.querySelector('.popup__btn_action_submit');
+
+// кнопка открытия попапа
+const popupOpenButton = document.querySelector('.profile__btn_action_edit');
+
+// имя профиля
+const profileName = document.querySelector('.profile__name');
+
+// профессия профиля
+const profileProfession = document.querySelector('.profile__profession');
+
+
+
+// функция открытия попапа
+const openPopup = function() {
+    popup.classList.toggle('popup_is-opened');
+    header.classList.toggle('header_is-shaded');
+    content.classList.toggle('content_is-shaded');
+    footer.classList.toggle('footer_is-shaded');
+    popupItems[0].value = profileName.textContent;
+    popupItems[1].value = profileProfession.textContent;
+};
+
+
+// функция закрытия попапа
+const closePopup = function() {
+    popup.classList.toggle('popup_is-opened');
+    header.classList.toggle('header_is-shaded');
+    content.classList.toggle('content_is-shaded');
+    footer.classList.toggle('footer_is-shaded');
+};
+
+
+// функция сохранения формы
+const submitForm = function(evt) {
+    evt.preventDefault();
+    profileName.textContent = popupItems[0].value;
+    profileProfession.textContent = popupItems[1].value;
+    closePopup();
+};
+
+
+// 'слушаем'
+popupOpenButton.addEventListener('click', openPopup);
+popupCloseButton.addEventListener('click', closePopup);
+popupSubmitButton.addEventListener('click', submitForm);
