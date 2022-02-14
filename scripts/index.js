6eb0138e2843067a5fd00b073f844c495404d9a8
rauskov <uskov.ra@yandex.ru>
2022-02-14 22:21:52 +0800

    вторая сборка

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,12 @@
-// Првет, тебе, мой ревьюер. 
-// Я пока не знаю кто ты, но если у тебя будут замечания, а скорее всего будут
-// Оставь, пожалуйста, координаты как с тобой связаться, чтобы можно было лучше устранить замечания
-// Обещаю не задалбливать вопросами
+// Саша, привет. Спасибо за ревью
+// Вроде все поправил, но везде где убрал height(а я его именно для этого и добалял), 
+// у меня текст поднялся и как его опустить я не знаю
+// Перелопалил кучу статей - тишина
+// Подскажи как полечить, плиз. https://disk.yandex.ru/i/g54pPhQIn0-c3w
 
 
-// хидер, контент, футер
-const header = document.querySelector('.header');
-const content = document.querySelector('.content');
-const footer = document.querySelector('.footer');
+// mask
+const mask = document.querySelector('.mask');
 
 // попап 
 const popup = document.querySelector('.popup');
@@ -15,11 +14,14 @@ const popup = document.querySelector('.popup');
 // кнопка закрытия попапа 
 const popupCloseButton = popup.querySelector('.popup__btn_action_close');
 
-// поля формы
-const popupItems = popup.querySelectorAll('.popup__item');
+// форма
+const popupName = popup.querySelector('.popup__name');
 
-// кнопка сохранения формы 
-const popupSubmitButton = popup.querySelector('.popup__btn_action_submit');
+// профессия формы
+const popupProfession = popup.querySelector('.popup__profession');
+
+// форма редактирования профиля
+const popupSubmitForm = popup.querySelector('.popup__form');
 
 // кнопка открытия попапа
 const popupOpenButton = document.querySelector('.profile__btn_action_edit');
@@ -35,28 +37,24 @@ const profileProfession = document.querySelector('.profile__profession');
 // функция открытия попапа
 const openPopup = function() {
     popup.classList.toggle('popup_is-opened');
-    header.classList.toggle('header_is-shaded');
-    content.classList.toggle('content_is-shaded');
-    footer.classList.toggle('footer_is-shaded');
-    popupItems[0].value = profileName.textContent;
-    popupItems[1].value = profileProfession.textContent;
+    mask.classList.toggle('mask_shade-on');
+    popupName.value = profileName.textContent;
+    popupProfession.value = profileProfession.textContent;
 };
 
 
 // функция закрытия попапа
 const closePopup = function() {
     popup.classList.toggle('popup_is-opened');
-    header.classList.toggle('header_is-shaded');
-    content.classList.toggle('content_is-shaded');
-    footer.classList.toggle('footer_is-shaded');
+    mask.classList.toggle('mask_shade-on');
 };
 
 
 // функция сохранения формы
 const submitForm = function(evt) {
     evt.preventDefault();
-    profileName.textContent = popupItems[0].value;
-    profileProfession.textContent = popupItems[1].value;
+    profileName.textContent = popupName.value;
+    profileProfession.textContent = popupProfession.value;
     closePopup();
 };
 
@@ -64,4 +62,4 @@ const submitForm = function(evt) {
 // 'слушаем'
 popupOpenButton.addEventListener('click', openPopup);
 popupCloseButton.addEventListener('click', closePopup);
-popupSubmitButton.addEventListener('click', submitForm);
+popupSubmitForm.addEventListener('submit', submitForm);
