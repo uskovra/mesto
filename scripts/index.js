b8060553fb51f6bb09fe70de04895d53b9ee3f29
rauskov <uskov.ra@yandex.ru>
2022-02-28 11:28:51 +0800

    5 sprint 3 iter

--- scripts/index.js
+++ scripts/index.js
@@ -54,94 +54,93 @@ const popupImageCloseButton = popupImage.querySelector('.popup__btn_action_close
 
 // функция создания карточки
 function createCard(name, link) {
-    const Card = cardTemplateContent.cloneNode(true);
+    const card = cardTemplateContent.cloneNode(true);
 
-    Card.querySelector('.card__text').textContent = name
-    Card.querySelector('.card__image').src = link
-    Card.querySelector('.card__image').alt = `фото ${name}`
+    card.querySelector('.card__text').textContent = name
+    card.querySelector('.card__image').src = link
+    card.querySelector('.card__image').alt = `фото ${name}`
 
-    Card.querySelector('.card__btn-like').addEventListener('click', LikeToggle);
-    Card.querySelector('.card__btn-delete').addEventListener('click', CardDelete);
-    Card.querySelector('.card__image').addEventListener('click', () => {
-        OpenPopup(popupImage), TransferDataToImagePopup(popupImage)
+    card.querySelector('.card__btn-like').addEventListener('click', llkeToggle);
+    card.querySelector('.card__btn-delete').addEventListener('click', cardDelete);
+    card.querySelector('.card__image').addEventListener('click', () => {
+        openPopup(popupImage), transferDataToImagePopup(popupImage)
     });
 
-    return Card
-}
+    return card
+};
 
 // функция добавления карточки
-function renderCard(Card) {
-    cardsList.prepend(Card)
-}
+function renderCard(card) {
+    cardsList.prepend(card)
+};
 
 // функция пролайкиавния
-function LikeToggle(event) {
+function llkeToggle(event) {
     event.target.classList.toggle('card__btn-like_type_on')
-}
+};
 
 // функция удаления карточки
-function CardDelete(event) {
+function cardDelete(event) {
     event.target.closest('.card').remove();
-}
+};
 
 // функция открытия попапа
-function OpenPopup(selector) {
+function openPopup(selector) {
     selector.classList.add('popup_is-opened')
-
-}
+};
 
 // функция закрытия попапа
-function ClosePopup(selector) {
+function closePopup(selector) {
     selector.classList.remove('popup_is-opened')
-}
+};
 
 //  функция очистки полей формы 
-function CleanFieldsAddPopup(selector) {
-    selector.querySelector('.popup__item_field_one').value = ''
-    selector.querySelector('.popup__item_field_two').value = ''
-}
+function cleanFieldsAddPopup(selector) {
+    selector.reset();
+};
 
 // функция переноса данных из профиля в попап
-function TransferDataToEditPopup(selector) {
-    selector.querySelector('.popup__item_field_one').value = profileName.textContent;
-    selector.querySelector('.popup__item_field_two').value = profileProfession.textContent;
-}
+function transferDataToEditPopup(selector) {
+    selector.profileName.value = profileName.textContent;
+    selector.profileJob.value = profileProfession.textContent;
+};
 
 // функция переноса данных из попапа в профиль
-function TransferDataToProfile(selector) {
-    profileName.textContent = selector.querySelector('.popup__item_field_one').value
-    profileProfession.textContent = selector.querySelector('.popup__item_field_two').value
+function transferDataToProfile(selector) {
+    profileName.textContent = selector.profileName.value
+    profileProfession.textContent = selector.profileJob.value
 };
 
 // функция переноса данных в попап с изображением
-function TransferDataToImagePopup(selector) {
+function transferDataToImagePopup(selector) {
+    text = event.target.closest('.card').querySelector('.card__text').textContent
     selector.querySelector('.popup__image').src = event.target.src
-    selector.querySelector('.popup__image').alt = `фото ${event.target.closest('.card').querySelector('.card__text').textContent}`
-    selector.querySelector('.popup__text').textContent = event.target.closest('.card').querySelector('.card__text').textContent
-}
+    selector.querySelector('.popup__image').alt = `фото ${text}`
+    selector.querySelector('.popup__text').textContent = text
+};
 
 
 // создаем начальные карточки
-initialCards.reverse().forEach(elem => renderCard(createCard(elem.name, elem.link)))
+initialCards.reverse().forEach(elem => renderCard(createCard(elem.name, elem.link)));
 
 
 
 // 'слушаем'
-popupEditProfileOpenButton.addEventListener('click', () => { OpenPopup(popupEditProfile), TransferDataToEditPopup(popupEditProfile) });
-popupEditProfileCloseButton.addEventListener('click', () => ClosePopup(popupEditProfile));
+popupEditProfileOpenButton.addEventListener('click', () => { openPopup(popupEditProfile), transferDataToEditPopup(popupEditProfileSubmitForm) });
+popupEditProfileCloseButton.addEventListener('click', () => closePopup(popupEditProfile));
 popupEditProfileSubmitForm.addEventListener('submit', () => {
     event.preventDefault();
-    TransferDataToProfile(popupEditProfile);
-    ClosePopup(popupEditProfile);
+    transferDataToProfile(popupEditProfileSubmitForm);
+    closePopup(popupEditProfile);
 });
 
 
-popupAddCardOpenButton.addEventListener('click', () => { OpenPopup(popupAddCard), CleanFieldsAddPopup(popupAddCard) });
-popupAddCardCloseButton.addEventListener('click', () => ClosePopup(popupAddCard));
+popupAddCardOpenButton.addEventListener('click', () => { openPopup(popupAddCard), cleanFieldsAddPopup(popupAddCardSubmitForm) });
+popupAddCardCloseButton.addEventListener('click', () => closePopup(popupAddCard));
 popupAddCardSubmitForm.addEventListener('submit', () => {
     event.preventDefault();
-    renderCard(createCard(popupAddCard.querySelector('.popup__item_field_one').value, popupAddCard.querySelector('.popup__item_field_two').value));
-    ClosePopup(popupAddCard);
+    renderCard(createCard(popupAddCardSubmitForm.placeName.value, popupAddCardSubmitForm.placeLink.value));
+    closePopup(popupAddCard);
 });
 
-popupImageCloseButton.addEventListener('click', () => ClosePopup(popupImage));
+popupImageCloseButton.addEventListener('click', () => closePopup(popupImage));
